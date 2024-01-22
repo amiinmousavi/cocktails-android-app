10acfe78f0bf18561ab038df4bd728ae8ea1d704
unknown <seyed.mousavi03@gmail.com>
2024-01-22 04:54:41 +0100

    added logo composable

--- app/src/main/java/be/aminmousavi/cocktailsapp/ui/utils/DefaultComposables.kt
+++ app/src/main/java/be/aminmousavi/cocktailsapp/ui/utils/DefaultComposables.kt
@@ -1,5 +1,6 @@
 package be.aminmousavi.cocktailsapp.ui.utils
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,12 +11,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import be.aminmousavi.cocktailsapp.R
 import be.aminmousavi.cocktailsapp.ui.theme.CocktailsAppTheme
+
+@Composable
+fun Logo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo),
+        contentDescription = stringResource(R.string.logo_png),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun CardButton(modifier: Modifier = Modifier) {
+
+}
 
 @Composable
 fun HeadingText(text: String, modifier: Modifier = Modifier) {
@@ -53,20 +71,24 @@ fun TitleText(text: String, modifier: Modifier = Modifier) {
         SubHeadingText(text = text, color = Color.White, modifier = Modifier.padding(2.dp))
     }
 }
+
 @Composable
 fun Paragraph(text: String, modifier: Modifier = Modifier) {
-    Text(text=text, modifier = Modifier.padding(0.dp, 4.dp, 0.dp, 12.dp))
+    Text(text = text, modifier = Modifier.padding(0.dp, 4.dp, 0.dp, 12.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CocktailsAppTheme {
-        Column(){
+        Column {
+            Logo()
             HeadingText("Heading Text")
             SubHeadingText(text = "Sub Heading Text")
             TitleText(text = "Title")
-            Paragraph(text = "Paragraph")
+            Paragraph(text = "Rub the rim of the glass with the lime slice to make the salt stick to it. " +
+                    "Take care to moisten only the outer rim and sprinkle the salt on it. " +
+                    "The salt should present to the lips of the imbiber and never mix into the cocktail.")
         }
     }
 }
