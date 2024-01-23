5c806983e8823de927d734dfb509328ec7ea87a3
unknown <seyed.mousavi03@gmail.com>
2024-01-23 13:42:26 +0100

    clean codebase

--- app/src/main/java/be/aminmousavi/cocktailsapp/network/DrinksApiService.kt
+++ app/src/main/java/be/aminmousavi/cocktailsapp/network/DrinksApiService.kt
@@ -1,14 +1,7 @@
 package be.aminmousavi.cocktailsapp.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-//import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 interface DrinksApiService {
     @GET("filter.php?a=Non_Alcoholic")
     suspend fun getNonAlcoholicDrinks(): DrinksApiResponse
-    //    @GET("photos")
-    //    suspend fun getNonAlcoholicDrinks(): List<MarsPhoto>
 }
