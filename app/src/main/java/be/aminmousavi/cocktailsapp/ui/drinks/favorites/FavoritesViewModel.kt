b36d9d8c746c66314f3bb5ef716370dc8bcb9162
unknown <seyed.mousavi03@gmail.com>
2024-02-02 18:22:39 +0100

    update UI state

--- app/src/main/java/be/aminmousavi/cocktailsapp/ui/drinks/favorites/FavoritesViewModel.kt
+++ app/src/main/java/be/aminmousavi/cocktailsapp/ui/drinks/favorites/FavoritesViewModel.kt
@@ -1,6 +1,28 @@
 package be.aminmousavi.cocktailsapp.ui.drinks.favorites
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import be.aminmousavi.cocktailsapp.data.CocktailsRepository
+import be.aminmousavi.cocktailsapp.model.Drink
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class FavoritesViewModel() : ViewModel() {
+class FavoritesViewModel(cocktailsRepository: CocktailsRepository) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val favoritesUiState: StateFlow<FavoritesUiState> =
+        cocktailsRepository.getAllDrinksStream().map {FavoritesUiState(it)}
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = FavoritesUiState()
+            )
+}
+
+data class FavoritesUiState(val drinkList: List<Drink> = listOf()){
+
 }
